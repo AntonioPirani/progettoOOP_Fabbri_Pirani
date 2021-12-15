82db2c3e91e224e4f4519eda13209cec5b03e1e9
Antonio Pirani <s1098080@studenti.univpm.it>
2021-12-15 12:35:04 +0100

    spring initializr

---
+++ WeatherApp/src/main/java/it/univpm/weather/WeatherApp/WeatherAppApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.weather.WeatherApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAppApplication.class, args);
+	}
+
+}
