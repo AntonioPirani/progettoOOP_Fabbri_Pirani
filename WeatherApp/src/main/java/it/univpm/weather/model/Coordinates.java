0bafd7456b8983487cdcef2a9553a0e14312611f
Matteo Fabbri <s1098015@studenti.univpm.it>
2021-12-19 16:33:46 +0100

    Scheletro Model: classe Coordinates

--- WeatherApp/src/main/java/it/univpm/weather/model/Coordinates.java
+++ WeatherApp/src/main/java/it/univpm/weather/model/Coordinates.java
@@ -1,5 +1,59 @@
 package it.univpm.weather.model;
 
-public class Coordinates {
+import java.util.Objects;
 
+
+/** La classe Coordinates descrive tutte le informazioni riguardanti le coordinate della citàà inserita.
+ * @author Matteo
+ */
+
+public class Coordinates 
+{
+	private double lat;
+	private double lon;
+	
+	/**
+	 * @param lat
+	 * @param lon
+	 */
+	public Coordinates(double lat, double lon) 
+	{
+		super();
+		this.lat = lat;
+		this.lon = lon;
+	}
+
+	/**
+	 * @return lat
+	 */
+	public double getLat() {
+		return lat;
+	}
+
+	/**
+	 * @param lat
+	 */
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+
+	/**
+	 * @return lon
+	 */
+	public double getLon() {
+		return lon;
+	}
+
+	/**
+	 * @param lon
+	 */
+	public void setLon(double lon) {
+		this.lon = lon;
+	}
+
+	@Override
+	public String toString() {
+		return "Coordinates [lat=" + lat + ", lon=" + lon + "]";
+	}
+	
 }
