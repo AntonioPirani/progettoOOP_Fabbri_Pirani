a44a52f0a30bf4db123172f022f2ad49cdc01478
Matteo Fabbri <s1098015@studenti.univpm.it>
2021-12-28 16:22:41 +0100

    Modifica exceptions

--- WeatherApp/src/main/java/it/univpm/weather/WeatherApp/exceptions/CityNotFoundException.java
+++ WeatherApp/src/main/java/it/univpm/weather/WeatherApp/exceptions/CityNotFoundException.java
@@ -11,7 +11,7 @@ public class CityNotFoundException extends Exception
 	/**
 	 * 
 	 */
-	//private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 	String output;
 
 	/** Costruttore con parametri.
