f75f48b4dbf9071d707eab95b9f6813703f07385
Matteo Fabbri <s1098015@studenti.univpm.it>
2022-01-09 14:55:30 +0100

    Aggiornamento TestCity

--- WeatherApp/src/test/java/it/univpm/weather/WeatherApp/testModel/TestCity.java
+++ WeatherApp/src/test/java/it/univpm/weather/WeatherApp/testModel/TestCity.java
@@ -1,5 +1,36 @@
 package it.univpm.weather.WeatherApp.testModel;
+import static org.junit.Assert.assertEquals;
 
-public class TestCity {
+import java.util.HashMap;
 
+import org.json.simple.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+
+import it.univpm.weather.WeatherApp.model.City;
+import it.univpm.weather.WeatherApp.model.Coordinates;
+import it.univpm.weather.WeatherApp.model.Temperature;
+
+public class TestCity 
+{
+	private Temperature temp ;
+	private City city;
+	private Coordinates coord;
+	
+	@Before
+	public void setUp()
+	{
+		temp = new Temperature(1641494587,6.78,5.39);
+		coord = new Coordinates(43.617,13.5171);
+		city = new City("Ancona", 3183087, 3600, coord, temp, null);
+	}
+	
+	@Test
+	public void testJSON()
+	{
+		String json = "{\"dateTime\":1641494587,\"feelsLike\":6.78,\"temp\":5.39,\"cityName\":\"Ancona\",\"timeZone\":3600,\"lon\":13.5170982,\"cityId\":3183087,\"lat\":43.6170137}";
+		
+		assertEquals(city.toJson(),json);
+	}
+		
 }
