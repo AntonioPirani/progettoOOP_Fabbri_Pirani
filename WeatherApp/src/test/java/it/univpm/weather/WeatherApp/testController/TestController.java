a7115a5e4dcb94222c1ae7a0ba84f2887ef28af6
Matteo Fabbri <s1098015@studenti.univpm.it>
2022-01-10 19:59:01 +0100

    Aggiornamento Test Controller and Statistics

--- WeatherApp/src/test/java/it/univpm/weather/WeatherApp/testController/TestController.java
+++ WeatherApp/src/test/java/it/univpm/weather/WeatherApp/testController/TestController.java
@@ -1,23 +1,36 @@
 package it.univpm.weather.WeatherApp.testController;
 
 import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.io.IOException;
 
 import org.json.simple.parser.ParseException;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import it.univpm.weather.WeatherApp.controller.TempController;
 import it.univpm.weather.WeatherApp.exceptions.HourException;
 
+@AutoConfigureMockMvc
 public class TestController 
 {
 	private TempController contro = new TempController();
+
+    @Autowired
+    private MockMvc mockMvc;
 	
 	@Test
-	public void testgetTemperature() throws HourException
+	public void testGetTemperature() throws Exception
 	{
-		assertNotNull(contro.getTemperature("Ancona"));		
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/current?cityName=Ancona")).andExpect(status().isOk());
 	}
 	
 	@Test
@@ -27,7 +40,7 @@ public class TestController
 	}
 	
 	@Test
-	public void testStatisttics() throws HourException 
+	public void testStatistics() throws HourException 
 	{
 		assertNotNull(contro.statistics("Ancona", "", 0));		
 	}
--- WeatherApp/src/test/java/it/univpm/weather/WeatherApp/testStats/TestStatistics.java
+++ WeatherApp/src/test/java/it/univpm/weather/WeatherApp/testStats/TestStatistics.java
@@ -1,5 +1,27 @@
 package it.univpm.weather.WeatherApp.testStats;
+import static org.junit.Assert.assertEquals;
 
-public class TestStatistics {
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import it.univpm.weather.WeatherApp.stats.Statistics;
 
+
+public class TestStatistics 
+{
+	
+	private Statistics stats;
+	
+	@BeforeAll
+	public void setUp()
+	{
+		stats = new Statistics(2.55,3.66,4.44,5.44);
+	}
+	
+	@Test
+	public void testJSON()
+	{
+		String json="";
+		assertEquals(stats.toJson(),json);
+	}
+	
 }
