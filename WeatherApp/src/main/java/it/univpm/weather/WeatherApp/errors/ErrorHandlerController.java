7cdf556e38976da07a7fc0b214e7e263dbc549ae
Matteo Fabbri <s1098015@studenti.univpm.it>
2022-01-19 10:15:11 +0100

    Aggiornamento ErrorHandelrController

--- WeatherApp/src/main/java/it/univpm/weather/WeatherApp/errors/ErrorHandlerController.java
+++ WeatherApp/src/main/java/it/univpm/weather/WeatherApp/errors/ErrorHandlerController.java
@@ -1,5 +1,8 @@
 package it.univpm.weather.WeatherApp.errors;
 
+import java.util.HashMap;
+
+import org.json.simple.JSONObject;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,72 +21,16 @@ public class ErrorHandlerController implements ErrorController{
 	 */
 	@RequestMapping("/error")
 	@ResponseBody
-	public String getErrorPath() {
-		String content="<<html>\r\n"
-				+ "	<style>\r\n"
-				+ "		table, th, td {\r\n"
-				+ "		border:1px solid black;\r\n"
-				+ "		border-collapse: collapse;\r\n"
-				+ "		border-radius: 83px;\r\n"
-				+ "		text-align:center;\r\n"
-				+ "		margin: 15px auto auto auto;\r\n"
-				+ "		color:#292929;\r\n"
-				+ "		}\r\n"
-				+ "		tr\r\n"
-				+ "		{\r\n"
-				+ "		margin: 7px 7px 7px 7px;\r\n"
-				+ "		}\r\n"
-				+ "	</style>\r\n"
-				+ "	<head>\r\n"
-				+ "		<title>Error Page</title>\r\n"
-				+ "		<title>Error Page</title>\r\n"
-				+ "		<link href=\"icon.png\">\r\n"
-				+ "	</head>\r\n"
-				+ "	<body style=\"background: -webkit-gradient(linear, left top, left bottom, from(#d9f9ff ), to(#fff)) fixed;\">\r\n"
-				+ "		<center><div style=\"border:2px solid grey; width:53%;  border-radius: 50px; margin-top:8px; background-color:#fffb96 ;\">\r\n"
-				+ "		<h2 style=\"text-align:center; color: #DE6806 ;\">PROGETTO PROGRAMMAZIONE OGGETTI</h2>\r\n"
-				+ "		<h3 style=\"text-align:center;color:#DE6806 ;margin-top:1px;\"><i>based on OpenWeather</i></h3></div></center>\r\n"
-				+ "		<table style=\"width:95%\">\r\n"
-				+ "			<tr>\r\n"
-				+ "				<th width = 7% style=\"font-size:25px\">Nome</th>\r\n"
-				+ "				<th width = 55% style=\"font-size:25px\">Esempio</th>\r\n"
-				+ "				<th width = 50% style=\"font-size:25px\">Descrizione</th>\r\n"
-				+ "			</tr>\r\n"
-				+ "			<tr>\r\n"
-				+ "				<td style=\"font-size:20px\">/</td>\r\n"
-				+ "				<td style=\"font-size:20px\">http://localhost:8080/</td>\r\n"
-				+ "				<td style=\"font-size:20px\"><div>Rotta di default/errore.</div></td>\r\n"
-				+ "			</tr>\r\n"
-				+ "			<tr>\r\n"
-				+ "				<td style=\"font-size:20px\">/current</td>\r\n"
-				+ "				<td style=\"font-size:20px\">http://localhost:8080/current?cityName=Ancona</td>\r\n"
-				+ "				<td style=\"font-size:20px\"><div>Rotta di tipo GET per ottenere la temperatura corrente di una citt&agrave, con la possibilit&agrave di salvare i dati con cadenza oraria.</div></td>\r\n"
-				+ "			</tr>\r\n"
-				+ "			<tr>\r\n"
-				+ "				<td style=\"font-size:20px\">/compare</td>\r\n"
-				+ "				<td style=\"font-size:20px\">http://localhost:8080/compare?cityName=Ancona&previousDay=1</td>\r\n"
-				+ "				<td style=\"font-size:20px\"><div>Rotta di tipo GET per confrontare le temperature correnti e percepite, in dato range tamporale in giorni.</div></td>\r\n"
-				+ "			</tr>\r\n"
-				+ "			<tr>\r\n"
-				+ "				<td style=\"font-size:20px\">/statistics</td>\r\n"
-				+ "				\r\n"
-				+ "				<td style=\"font-size:20px\">\r\n"
-				+ "					<ol style=\"text-align:justify;margin-top:12px;\">"
-				+ "						<li> http://localhost:8080/statistics?cityName=Ancona</li>\r\n"
-				+ "						<li> http://localhost:8080/statistics?cityName=Ancona&filterBy=\"hour\"&time=1</li>\r\n"
-				+ "						<li>http://localhost:8080/statistics?cityName=Ancona&filterBy=\"day\"&time=1</li>\r\n"
-				+ "						<li>http://localhost:8080/statistics?cityName=Ancona&filterBy=\"week\"&time=1</li>\r\n"
-				+ "					</ol>\r\n"
-				+ "				</td>\r\n"
-				+ "				<td style=\"font-size:20px\"> <div>Rotta di tipo GET per restituire il filtraggio delle statistiche in base alla periodicit&agrave: fascia oraria, giornaliera e settimanale. Inoltre, se il filtro non viene specificato, viene analizzato tutto lo storico.</div></td>\r\n"
-				+ "			</tr>\r\n"
-				+ "		</table>\r\n"
-				+ "		<center><img src=\"icon.png\" style=\"width:88px;height:88px; margin-top:20px;\"></center>\r\n"
-				+ "		<footer style=\"margin-top:0px\">\r\n"
-				+ "			<p style=\"text-align:center;\"><i>Created by Antonio Pirani & Matteo Fabbri. &copy 2021/22</i></p>\r\n"
-				+ "		</footer>\r\n"
-				+ "	</body>\r\n"
-				+ "</html>";
-		return content;		
+	public JSONObject getErrorPath() {
+		
+		HashMap<String,Object> map = new HashMap<String,Object>();
+		
+		map.put("error","Il comando inserito non è stato riconosciuto");
+		map.put("errorPage","https://htmlpreview.github.io/?https://github.com/AntonioPirani/progettoOOP_Fabbri_Pirani/blob/main/WeatherApp/src/main/resources/static/errorPage.html");
+		map.put("reeadMe", "https://github.com/AntonioPirani/progettoOOP_Fabbri_Pirani");
+		
+		JSONObject output = new JSONObject(map);
+		
+		return output;
 	}
 }
